c5418b10592a8abe2348bda46f807a08683e8e2d
Terrance Niechciol <TNiechciol@gmail.com>
2017-05-14 20:37:51 -0400

    WIP: new resume style

--- PageStructure.hs
+++ PageStructure.hs
@@ -10,6 +10,7 @@ module PageStructure
 , mainLayout
 , exactly
 , image
+, inlineSvg
 , screenshot
 , youtube_video
 , projectSection
@@ -211,6 +212,11 @@ image url alt = do
                   ,("alt", alt)
                   ,("title", alt)
                   ,("onload", "this.parentNode.classList.remove('loading')")] noHtml
+
+inlineSvg file = do
+    contents <- lift $ readFile file
+    tag "div" [("class", "image_container loading")] $ do
+        tag "svg" [] $ uText contents
 
 screenshot url alt =
     tag "div" [("class", "screenshot_container")] $ do
